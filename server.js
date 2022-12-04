8c97e09e5ae0f975e03c3b7298cfe5075ead1163
Dave Berzack <daveberzack@gmail.com>
2022-12-04 10:57:34 -0500

    secure creds

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const AWS = require("aws-sdk");
 const express = require("express");
 const bodyParser = require("body-parser");
@@ -13,37 +14,29 @@ app.use(cors());
 app.use(bodyParser.json({limit: '5mb'}));
 app.use(bodyParser.urlencoded({limit: '5mb', extended: true }));
 
-// const s3 = new AWS.S3({
-//   accessKeyId: process.env.AWS_S3_ACCESS_KEY_ID,
-//   secretAccessKey: process.env.AWS_S3_SECRET_ACCESS_KEY,
-// })
 const s3 = new AWS.S3({
-  accessKeyId: "AKIAXBHKRV7MWNVMV2U3",
-  secretAccessKey: "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",
-  region:"us-east-1"
+  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
 })
-//const s3Bucket = new AWS.S3( { params: {Bucket: 'hocus-focus'} } );
-
+// const s3 = new AWS.S3({
+//   accessKeyId: "AKIAXBHKRV7MWNVMV2U3",
+//   secretAccessKey: "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",
+//   region:"us-east-1"
+// })
 
 const generateRandomString = (myLength) => {
-  const chars =
-    "AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz1234567890";
+  const chars ="AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz1234567890";
   const randomArray = Array.from(
     { length: myLength },
     (v, k) => chars[Math.floor(Math.random() * chars.length)]
   );
-
   const randomString = randomArray.join("");
   return randomString;
 };
 
 const putImage = async (image) => {
-
-  
   const base64Data = new Buffer.from(image.replace(/^data:image\/\w+;base64,/, ""), 'base64');
-
   const type = image.split(';')[0].split('/')[1];
-
   const fileKey = generateRandomString(10);
 
   const params = {
@@ -56,34 +49,13 @@ const putImage = async (image) => {
   }
 
   let location = '';
-  let key = '';
   try {
-    const { Location, Key } = await s3.upload(params).promise();
-    location = Location;
-    key = Key;
+    const { loc } = await s3.upload(params).promise();
+    location = loc;
   } catch (error) {
     console.log("put image error",error);
   }
-
-  console.log(location, key);
-
   return location;
-
-  // var buf = Buffer.from(image.replace(/^data:image\/\w+;base64,/, ""),'base64')
-  // var data = {
-  //   Key: req.body.userId, 
-  //   Body: buf,
-  //   ContentEncoding: 'base64',
-  //   ContentType: 'image/jpeg'
-  // };
-  // s3Bucket.putObject(data, function(err, data){
-  //     if (err) { 
-  //       console.log(err);
-  //       console.log('Error uploading data: ', data); 
-  //     } else {
-  //       console.log('successfully uploaded the image!');
-  //     }
-  // });
 }
 
 mongoose.connect("mongodb+srv://daveberzack:zNDEUMMtWlnmRZYi@cluster0.rddcyey.mongodb.net/?retryWrites=true&w=majority", {
@@ -195,13 +167,12 @@ app.post("/hocuschallenge", async (request, response) => {
 
 app.post("/hocuschristmas", async (request, response) => {
   const bodyData = request.body;
-  console.log("XMas Data:",bodyData);
   const imageUrl = await putImage(bodyData.image);
 
   const challengeData = {
     clue: bodyData.clue,
     imageUrl,
-    hitAreas: bodyData.strokes,
+    hitAreas: bodyData.hitAreas,
     goals: [20,40,60,90,120],
     beforeMessages: [
       {
@@ -273,7 +244,7 @@ app.post("/fiveminuteadd", async (request, response) => {
 });
 
 app.get("/", async (request, response) => {
-  response.send("Hello.");
+  response.send("Hello."+process.env.AWS_ACCESS_KEY_ID);
 });
 
 app.listen(PORT, () => {
